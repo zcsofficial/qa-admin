900af745b35754a9bf970e6ab1ce1f8b192220b0
zcsofficial <contact.zcsco@gmail.com>
2024-10-27 14:40:19 +0530

    unwanted things delted

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        $("#sidebar").toggleClass("d-none");
-    });
-});
